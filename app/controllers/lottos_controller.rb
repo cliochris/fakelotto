ae16c1bf59dc4c1532b48cb0449f0f553105aff4
Chris Thompson <chris@goclio.com>
2016-04-20 08:23:05 -0600

    Create the controller to generate lotto numbers.

---
+++ app/controllers/lottos_controller.rb
@@ -0,0 +1,16 @@
+class LottosController < ApplicationController
+  SEED = 5432
+  PICK = 6
+  OUT_OF = 49
+  def index
+    numbers = []
+    PICK.times.each do
+      numbers << generator.rand(OUT_OF)
+    end
+    render text: numbers.join("\n") + "\n"
+  end
+
+  private def generator
+    @@generator ||= Random.new(SEED)
+  end
+end
